788067d7edb530351dbfac271ab15f53ccd851fb
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-01-28 23:03:41 -0500

    add animation on about

--- static/js/aboutAnimateCss.js
+++ static/js/aboutAnimateCss.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $('.bio').addClass("hidden").viewportChecker({
     classToAdd: "visible animated fadeIn",
     });
+  $('.bio-content').addClass("hidden").viewportChecker({
+    classToAdd: "visible animated slideInRight",
+    });
 });
--- static/js/map.js
+++
@@ -1,9 +0,0 @@
-var map;
-var location;
-  function initMap() {
-    location = {lat: 40.560, lng: -85.689};
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: location,
-      zoom: 4,
-    });
-  }
